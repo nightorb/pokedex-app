88759f349ce2f813bed6af878af35cca15ab1f25
nightorb <89855337+nightorb@users.noreply.github.com>
2021-09-09 16:02:59 +0200

    added a pokemonList to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
-alert('Hello World');
-
-let favoriteFood = 'Banana';
-document.write(favoriteFood);
-favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['Grass','Poison'] },
+  { name: 'Charmander', height: 0.6, weight: 8.5, type: ['Fire'] },
+  { name: 'Squirtle', height: 0.5, weight: 9, type: ['Water'] },
+  { name: 'Gastly', height: 1.3, weight: 0.1, type: ['Ghost','Poison'] },
+  { name: 'Haunter', height: 1.6, weight: 0.1, type: ['Ghost','Poison'] }, // Haunter is my favorite Pokémon
+  { name: 'Gengar', height: 1.5, weight: 40.5, type: ['Ghost','Poison'] },
+];
