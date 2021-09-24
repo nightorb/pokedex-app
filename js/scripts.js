d74d33b10f3d2d739ba9bab7dff62137dda7e925
nightorb <89855337+nightorb@users.noreply.github.com>
2021-09-24 21:24:11 +0200

    added functions to load data from external source
    pokemonList fetches data from pokeapi, then displays each pokemon in list. details of pokemon are logged to console on-click.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@
 let pokemonRepository = (function() {
   // pokemonList only accessible through the functions returned by IIFE
   let pokemonList = [];
+  let apiURL = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
   
   function getAll() {
     return pokemonList;
@@ -9,16 +10,8 @@ let pokemonRepository = (function() {
 
   // adds only objects to pokemonList and checks wheter it includes the required keys
   function add(pokemon) {
-    typeof pokemon === 'object' && 'name' in pokemon && 'height' in pokemon && 'weight' in pokemon && 'type' in pokemon ?
-    pokemonList.push(pokemon) : console.log('Pokémon is invalid.');
-
-    /* "advanced" version:
-    const props = ['name','height','weight','type'];
-    const checkObject = Object.keys(pokemon).reduce((accumulator, current) => {
-      if(!accumulator) return accumulator;
-      return props.includes(current)}, true);
-    typeof pokemon === 'object' && checkObject ? pokemonList.push(pokemon) : !pokemonList.push() + console.log('Pokémon is invalid.');
-    */
+    typeof pokemon === 'object' && 'name' in pokemon && 'detailsURL' in pokemon ?
+      pokemonList.push(pokemon) : console.log('Pokémon is invalid.');
   }
 
   // adds li to the HTML/DOM
@@ -33,36 +26,98 @@ let pokemonRepository = (function() {
     buttonEvent(button, pokemon);
   }
 
-  // separate function for button adding event listener, logs Pokémon's details on-click
+  // separate function for adding event listener to button, logs pokémon's details on-click
   function buttonEvent(button, pokemon) {
     button.addEventListener('click', () => showDetails(pokemon))
   }
 
   function showDetails(pokemon) {
-    console.log(pokemon.name);
+    loadDetails(pokemon).then(function() {
+      console.log(pokemon);
+    });
   }
+
+  /* attempt to add a loading message
+  let loadingMessage = (function() {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let loadingMessage = document.createElement('p');
+    loadingMessage.innerText = 'Loading Pokédex...';
+    
+    function showLoadingMessage() {
+      pokemonList.appendChild(loadingMessage);
+    };
+    
+    function hideLoadingMessage() {
+      document.querySelector(loadingMessage);
+      loadingMessage.pokemonList.removeChild(loadingMessage);
+    };
+    
+    return {
+      showLoadingMessage: showLoadingMessage,
+      hideLoadingMessage: hideLoadingMessage
+    };
+  })();
+  */
+
+  // loadList fetches data from api
+  function loadList() {
+    
+    //!fetch(apiURL) ? loadingMessage.showLoadingMessage() : !loadingMessage.showLoadingMessage();
+    
+    return fetch(apiURL).then(function(response) {
+    
+    //loadingMessage.hideLoadingMessage();
+      
+    return response.json();
+    }).then(function(json) {
+      
+      //loadingMessage.hideLoadingMessage();
+      
+      json.results.forEach(function(item) {
+        // each item should have name and detailsURL property
+        // use detailsURL to load detailed data for a given pokémon in loadDetails
+        let pokemon = {
+          name: item.name,
+          detailsURL: item.url
+        };
+        // each pokémon in fetched data is added to pokemonList with add function
+        add(pokemon);
+      });
+    }).catch(function(e) {
+      
+      // loadingMessage.hideLoadingMessage();
+      
+      console.error(e);
+    });
+  }
+
+  function loadDetails(pokemon) {
+    let url = pokemon.detailsURL;
+    return fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(details) {
+      // now adding details to the pokémon item
+      pokemon.imageURL = details.sprites.front_default;
+      pokemon.height = details.height;
+      pokemon.types = details.types;
+    }).catch(function(e) {
+      console.error(e);
+    });
+  }  
 
   return {
     getAll: getAll,
     add: add,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails
   };
 })();
 
-//will be added since it's an object
-pokemonRepository.add(
-  {
-    name: 'Dragonite',
-    height: 2.2,
-    weight: 210,
-    type: ['Dragon','Flying']
-  }
-);
-
-// will not be added since it's a string
-pokemonRepository.add('Hello');
-
-// forEach function that iterates over each Pokémon
-pokemonRepository.getAll().forEach(function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  // now the data is loaded
+  // forEach function that iterates over each pokémon
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
